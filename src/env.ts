8eaf9eaf931694473414f956346851499453bfbf
rudrodip <official.rudrodipsarker@gmail.com>
2025-05-14 22:01:24 +0600

    t3 env

---
+++ src/env.ts
@@ -0,0 +1,17 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+ 
+export const env = createEnv({
+  server: {
+    // BetterAuth
+    BETTER_AUTH_URL: z.string().min(1),
+    BETTER_AUTH_SECRET: z.string().min(1),
+    // Database
+    DATABASE_URL: z.string().min(1),
+    // Github
+    GITHUB_CLIENT_ID: z.string().min(1),
+    GITHUB_CLIENT_SECRET: z.string().min(1),
+  },
+  client: {},
+  experimental__runtimeEnv: {},
+});
