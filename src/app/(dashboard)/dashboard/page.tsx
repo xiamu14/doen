b1817563398d9a1145c760d4f88cb25cb23e732f
rudrodip <official.rudrodipsarker@gmail.com>
2025-05-16 01:11:21 +0600

    skeleton loading

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,7 +1,6 @@
 import { getUser } from "@/lib/auth-utils";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import { Icons } from "@/components/icons";
 import { Github } from "lucide-react";
 import { siteConfig } from "@/config/site.config";
 import Link from "next/link";
