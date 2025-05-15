8379a36068e750be4e31eeffe5506c20b3a1d287
rudrodip <official.rudrodipsarker@gmail.com>
2025-05-15 20:57:57 +0600

    improved styling

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -26,7 +26,7 @@ export default function DashboardLayout({
 const MockNavbar = () => {
   return (
     <div id="nav" className="border-b border-dashed flex items-center justify-between">
-      <div id="brand" className="h-full border-r border-dashed w-[300px] flex items-center justify-center">
+      <div id="brand" className="h-full md:border-r border-dashed w-[300px] flex items-center justify-center">
         <Button variant="ghost" className="w-full h-full font-heading text-2xl font-bold" asChild>
           <Link href="/">
             <span>{siteConfig.name}</span>
@@ -34,6 +34,15 @@ const MockNavbar = () => {
         </Button>
       </div>
       <div className="flex-1 flex items-center justify-end h-full border-dashed divide-x">
+      <Button className="h-full border-l border-dashed" size="lg" variant="ghost" asChild>
+          <a href={siteConfig.socials.x} target="_blank" className="flex items-center gap-2 group/nav">
+            <span>X</span>
+            <div className="relative z-10 size-4 overflow-hidden flex items-center justify-center">
+              <ArrowUpRight className="-z-10 absolute opacity-100 scale-100 left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 group-hover/nav:-translate-y-5 group-hover/nav:translate-x-5 group-hover/nav:opacity-0 group-hover/nav:scale-0 transition-all duration-200" />
+              <ArrowUpRight className="absolute -z-10 -bottom-4 -left-4 opacity-0 scale-0 group-hover/nav:-translate-y-[15px] group-hover/nav:translate-x-4 group-hover/nav:opacity-100 group-hover/nav:scale-100 transition-all duration-200" />
+            </div>
+          </a>
+        </Button>
         <Button className="h-full border-l border-dashed" size="lg" variant="ghost" asChild>
           <a href={siteConfig.socials.github} target="_blank" className="flex items-center gap-2 group/nav">
             <span>Github</span>
--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -67,7 +67,7 @@ export default function Home() {
               <p className="text-muted-foreground max-w-3xl">{siteConfig.description}</p>
             </div>
             <div id="code" className="flex flex-col p-4">
-              <div className="p-2 border border-dashed bg-card text-sm flex items-center justify-between">
+              <div className="p-2 border border-dashed hover:border-primary/50 bg-card text-sm flex items-center justify-between transition-all duration-200 delay-75">
                 <pre className="font-mono bg-linear-to-r from-muted-foreground to-foreground bg-clip-text text-transparent">
                   git clone {siteConfig.socials.github}
                 </pre>
@@ -77,7 +77,7 @@ export default function Home() {
               </div>
             </div>
             <div id="cta" className="flex items-center gap-4 p-4">
-              <Button variant="outline" asChild className="relative">
+              <Button variant="outline" asChild className="relative border-dashed">
                 <a href={siteConfig.socials.github} target="_blank" className="gap-2 group">
                   <div className="w-full h-[1px] bg-linear-to-r from-primary/0 via-primary to-primary/0 absolute top-0 -left-1/2 translate-x-1/2 opacity-0 group-hover:opacity-100 transition-all duration-500"></div>
                   <Github className="size-4" />
@@ -102,8 +102,15 @@ export default function Home() {
               className={cn(
                 "relative w-full p-6 hover:bg-muted/50 transition-all duration-150 group/item border-dashed",
                 {
-                  "border-b": index < techConfig.length / 2,
-                  "border-r": index % 3 !== 2,
+                  "border-b": index < techConfig.length - 1,
+                  "md:border-b-0": index >= techConfig.length - 2,
+                  "md:border-b": index < techConfig.length - 2,
+                  "lg:border-b-0": index >= techConfig.length - 3,
+                  "lg:border-b": index < techConfig.length - 3,
+                },
+                {
+                  "md:border-r": index % 2 === 0 && index !== techConfig.length - 1,
+                  "lg:border-r": index % 3 !== 2 && index !== techConfig.length - 1,
                 }
               )}
             >
