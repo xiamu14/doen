c6ec8c4886afb814274f2818716f2fc0cb1cba86
rudrodip <official.rudrodipsarker@gmail.com>
2025-05-15 18:52:38 +0600

    auth pages and custom layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,18 @@
+import { Goku } from "@/app/(marketing)/page";
+
 export default function AuthLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
-    return <div className="flex flex-col items-center justify-center h-screen">{children}</div>;
+    return (
+        <div className="flex items-center justify-between h-screen">
+            <div className="w-1/2 h-full bg-black">
+                <Goku />
+            </div>
+            <div className="w-1/2 h-full flex items-center justify-center">
+                {children}
+            </div>
+        </div>
+    );
 }
